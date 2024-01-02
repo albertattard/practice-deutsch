92ce80eba1ad696895f458c0bc6df3b1ba957144
Albert Attard <albert.attard@oracle.com>
2024-01-02 13:29:59 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::io::stdin;
-
 use clap::Parser;
-use rand::seq::SliceRandom;
 
 use crate::audio::pronounce;
-use crate::nouns::Noun;
+use crate::nouns::articles;
 
 mod audio;
 mod download;
@@ -39,53 +36,4 @@ fn plural() {
 
 fn verbs() {
     println!("Not implemented yet");
-}
-
-fn articles() {
-    let nouns: Vec<Noun> = Noun::read_nouns().expect("Failed to read nouns");
-
-    loop {
-        let mut rng = rand::thread_rng();
-        let noun = nouns.choose(&mut rng).unwrap();
-
-        let question = &noun.random_question();
-        println!("{} ({}): ", question.noun, question.english);
-
-        question.play();
-
-        loop {
-            let mut input = String::new();
-            stdin().read_line(&mut input).expect("Failed to user input");
-
-            let input = &input.trim().to_ascii_lowercase();
-            match input.as_str() {
-                "q" | "quit" => return,
-                "r" | "repeat" => {
-                    question.play();
-                    continue;
-                }
-                "die" | "der" | "das" => {
-                    if !question.article.contains(input) {
-                        print!("Wrong! ");
-                        question.play_with_article();
-                    };
-                    break;
-                }
-                _ => {
-                    println!("Expected the articles der, die or das");
-                    println!("         q or quit to quit");
-                    println!("         r or repeat to replay the audio");
-                    continue;
-                }
-            }
-        }
-
-        print!("Correct answer: {}", question.article[0]);
-        if question.article.len() > 1 {
-            print!(" (or {})", question.article[1]);
-        }
-        println!(" {}", question.noun);
-
-        question.play_with_article();
-    }
 }
--- src/nouns.rs
+++ src/nouns.rs
@@ -1,29 +1,80 @@
 use crate::audio::play_file;
 use crate::download::download_file;
+use rand::prelude::SliceRandom;
 use rand::Rng;
 use std::error::Error;
+use std::io::stdin;
 use std::path::{Path, PathBuf};
 
+pub(crate) fn articles() {
+    let nouns: Vec<Noun> = Noun::read_nouns().expect("Failed to read nouns");
+
+    loop {
+        let mut rng = rand::thread_rng();
+        let noun = nouns.choose(&mut rng).unwrap();
+
+        let question = &noun.random_question();
+        println!("{} ({}): ", question.noun, question.english);
+
+        question.play();
+
+        loop {
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed to user input");
+
+            let input = &input.trim().to_ascii_lowercase();
+            match input.as_str() {
+                "q" | "quit" => return,
+                "r" | "repeat" => {
+                    question.play();
+                    continue;
+                }
+                "die" | "der" | "das" => {
+                    if !question.article.contains(input) {
+                        print!("Wrong! ");
+                        question.play_with_article();
+                    };
+                    break;
+                }
+                _ => {
+                    println!("Expected the articles der, die or das");
+                    println!("         q or quit to quit");
+                    println!("         r or repeat to replay the audio");
+                    continue;
+                }
+            }
+        }
+
+        print!("Correct answer: {}", question.article[0]);
+        if question.article.len() > 1 {
+            print!(" (or {})", question.article[1]);
+        }
+        println!(" {}", question.noun);
+
+        question.play_with_article();
+    }
+}
+
 #[derive(Debug, serde::Deserialize)]
-pub(crate) struct Noun {
+struct Noun {
     english: String,
     article: String,
     singular: String,
     plural: Option<String>,
 }
 
-pub(crate) struct NounQuestion {
-    pub(crate) english: String,
-    pub(crate) noun: String,
-    pub(crate) article: Vec<String>,
-    pub(crate) file_path: PathBuf,
-    pub(crate) with_article_file_path: PathBuf,
-    pub(crate) file_link: String,
-    pub(crate) with_article_file_link: String,
+struct NounQuestion {
+    english: String,
+    noun: String,
+    article: Vec<String>,
+    file_path: PathBuf,
+    with_article_file_path: PathBuf,
+    file_link: String,
+    with_article_file_link: String,
 }
 
 impl Noun {
-    pub(crate) fn read_nouns() -> Result<Vec<Noun>, Box<dyn Error>> {
+    fn read_nouns() -> Result<Vec<Noun>, Box<dyn Error>> {
         let reader = csv::Reader::from_path("nouns.csv");
 
         let nouns: Vec<Noun> = reader?.deserialize().map(|r| r.unwrap()).collect();
@@ -57,7 +108,7 @@ impl Noun {
         )
     }
 
-    pub(crate) fn random_question(&self) -> NounQuestion {
+    fn random_question(&self) -> NounQuestion {
         let mut rng = rand::thread_rng();
 
         if rng.gen_bool(1.0) {
@@ -113,11 +164,11 @@ impl Noun {
 }
 
 impl NounQuestion {
-    pub(crate) fn play(&self) {
+    fn play(&self) {
         download_if_missing_and_play(self.file_path.as_path(), &self.file_link);
     }
 
-    pub(crate) fn play_with_article(&self) {
+    fn play_with_article(&self) {
         download_if_missing_and_play(
             self.with_article_file_path.as_path(),
             &self.with_article_file_link,
