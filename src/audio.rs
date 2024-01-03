db1adf6e44370c48a0281d047e0e323ff6e105c0
Albert Attard <albert.attard@oracle.com>
2024-01-03 14:13:56 +0100

    Use the proper way to wait for the audio to finish

--- src/audio.rs
+++ src/audio.rs
@@ -1,14 +1,11 @@
 use rand::Rng;
-use std::cmp::max;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::{stdin, BufReader};
 use std::path::{Path, PathBuf};
-use std::thread::sleep;
-use std::time::Duration;
 
-use rodio::{source::Source, Decoder, OutputStream};
+use rodio::{Decoder, OutputStream, Sink};
 
 pub(crate) fn pronounce(directory: &str) {
     let mut files = Vec::new();
@@ -80,14 +77,12 @@ pub(crate) fn play_file(path: &Path) -> Result<(), Box<dyn Error>> {
 
     let (_stream, stream_handle) = OutputStream::try_default()?;
     let file = File::open(path)?;
-    let length = file.metadata().unwrap().len();
     let reader = BufReader::new(file);
     let source = Decoder::new(reader)?;
-    stream_handle.play_raw(source.convert_samples())?;
-
-    /* The sound plays in a separate audio thread, so we need to keep the main thread alive while it's playing.
-    The file size is used as an approximation of the audio length. */
-    sleep(Duration::from_millis(max(length as u64 / 10, 1_750)));
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
+    /* The sound plays in a separate audio thread, so we need to keep the main thread alive while it's playing. */
+    sink.sleep_until_end();
 
     Ok(())
 }
